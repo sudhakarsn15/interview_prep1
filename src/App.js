e266b104e8c1c2a525a71073269c70e212341678
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2022-03-11 09:00:14 +0530

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [data, setData] = useState("");
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log(data);
+  });
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((response) => response.json())
+      .then((result) => {});
+  };
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>1. Fucntion to Fetch data from given URL </h2>
     </div>
   );
 }
